4b1da82cba8b22565777bacee17535e3cf1eb3fd
Prudvi <prudvirajvarma2004@gmail.com>
2025-02-23 20:02:08 +0530

    Added Middlewares

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,34 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt'
+import { jwt, sign, verify } from 'hono/jwt'
+
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string,
     JWT_SECRET: string
   }
 }>()
+
+app.use("/api/blog/*", async (c, next) => {
+  const token = c.req.header('Authorization') || ""
+
+  if (!token) {
+    return c.json({ error: "Unauthorized: No token provided" }, 401)
+  }
+
+  try {
+    const response = await verify(token, c.env.JWT_SECRET)
+
+    if (!response.id) {
+      return c.json({ error: "Unauthorized: Invalid token" }, 403)
+    }
+
+    await next()
+  } catch (error) {
+    return c.json({ error: "Unauthorized: Token verification failed" }, 403)
+  }
+})
 
 
 
