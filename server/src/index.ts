651c0e09bbf10080e2662c976067f4ccfbd5e8a7
Prudvi <prudvirajvarma2004@gmail.com>
2025-02-23 12:50:25 +0530

    Added DB config and models

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,37 @@
+import 'dotenv/config'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/signup', (c) => {
+  return c.text('Signup')
+})
+
+app.post("/api/login",(c) => {
+  return c.json("Login")
+})
+
+app.post("/api/blog",(c) => {
+  return c.json("Create Blog")
+})
+
+app.get("/api/blog/:id",(c) => {
+  const { id } = c.req.param()
+  return c.json("Get Blog")
+})
+
+app.put("/api/blog/:id",(c) => {
+  const {id} = c.req.param()
+  return c.json("Edit Blog")
+})
+
+app.delete("/api/blog/:id",(c) => {
+  const {id} = c.req.param()
+  return c.json("Delete Blog")
+})
+
+app.get("/api/blog",(c) => {
+  return c.text("All Blogs")
 })
 
 export default app
